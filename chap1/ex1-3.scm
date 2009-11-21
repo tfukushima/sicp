293787b90905d47dacaf32f8bf31b5f8ee242009
福島 拓 <tfukushima@fukushima-taku-no-macbook.local>
2009-11-21 16:15:27 +0900

    [Delete] categorize some files

--- chap1/ex1-3.scm
+++
@@ -1,4 +0,0 @@
-(define (sum3 x y z)
-  (if (< x y)
-      (if (< x z) (+ y z) (+ x y))
-      (if (< y z) (+ x z) (+ x y))))
--- chap1/ex1-6.scm
+++
@@ -1,9 +0,0 @@
-(define (new-if predicate then-clause else-caluse)
-  (cond (predicate then-clause)
-	(eles else-caluse)))
-
-(define (sqrt-iter guess x)
-  (new-if (good-enough? guess x)
-	  guess
-	  (sqrt-iter (improve guess x)
-		     x)))
