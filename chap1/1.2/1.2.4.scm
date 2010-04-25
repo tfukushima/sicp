95a2ad040c6cccd231b14b83e8cf3e5532910ffe
Taku Fukushima <f.tac.mac@gmail.com>
2010-04-25 13:35:31 +0900

    [Add] 1.2.4 Exponentiation

---
+++ chap1/1.2/1.2.4.scm
@@ -0,0 +1,28 @@
+;; Naive exponentiation
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+;; Iterative exponentiation
+(define (expt b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+                 (- counter 1)
+                 (* b product))))
+
+;; Fast exponentiation
+(define (square x)
+  (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
